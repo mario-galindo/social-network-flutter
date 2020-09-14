4d2ff409fe07aa7ea831b407b969435a649a6441
mario-galindo <mgalindo9410@gmail.com>
2020-09-14 01:33:44 -0600

    add login page with google

--- lib/pages/HomePage.dart
+++ lib/pages/HomePage.dart
@@ -7,8 +7,57 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  bool isSignedIn = false;
+
+  Widget buildHomeScreen() {
+    return Text("already signined in");
+  }
+
+  Scaffold buildSignInScreen() {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+                begin: Alignment.topRight,
+                end: Alignment.bottomLeft,
+                colors: [
+              Theme.of(context).accentColor,
+              Theme.of(context).primaryColor
+            ])),
+        alignment: Alignment.center,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              "BudiesGram",
+              style: TextStyle(
+                  fontSize: 92.0, color: Colors.white, fontFamily: "Signatra"),
+            ),
+            GestureDetector(
+              onTap: () => "Boton Presionado",
+              child: Container(
+                width: 270.0,
+                height: 65.0,
+                decoration: BoxDecoration(
+                    image: DecorationImage(
+                        image: AssetImage(
+                            "assets/images/google_signin_button.png"),
+                        fit: BoxFit.cover)),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Text("Here goes Home Page");
+    if (isSignedIn) {
+      return buildHomeScreen();
+    } else {
+      return buildSignInScreen();
+    }
   }
 }
